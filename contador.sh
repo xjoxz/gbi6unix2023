80b5acf59665eb493dd02e3624e5e22a47021597
Moises Gualapuro <43993933+gualapuromoises@users.noreply.github.com>
2023-04-28 05:50:25 +0000

    Identfificadores en contador.sh

--- contador.sh
+++ contador.sh
@@ -1,3 +1,3 @@
 #!/bin/bash/
-head -n 20 Pacifici2013_data.csv | cut -d ";" -f 2-6 | tr ";" " " > animales.csv
-cat animales.csv| cut -d " " -f 1| sort |uniq -c
+tail -n +2 $1 | cut -d ";" -f 2-6 | tr ";" " " > $2
+cat $2 | cut -d " " -f 1| sort |uniq -c > $3
